b3b50982b7a63b1a998f01bc6386a45fc32a5f11
Farzin <farzinahmadzade909@gmail.com>
2025-12-23 14:36:37 +0330

    Update read_LiCSBAS-HDF5.py

--- read_LiCSBAS-HDF5.py
+++ read_LiCSBAS-HDF5.py
@@ -1,16 +1,18 @@
 import h5py
 import matplotlib.pyplot as plt
 import csv
+from pathlib import Path
 
-h5_path = "000002_028A_05385_191813_filt.hdf5"
+data_dir = Path(r"K:\GitHub\Geo-Deformation-Lab\Data")
+h5_path = data_dir / "000002_028A_05385_191813_filt.hdf5"
 
 with h5py.File(h5_path, "r") as f:
     print("keys:", list(f.keys()))
 
-    disp = f["displacement"][()]      # (time, y, x)
-    dates = f["date"][()]             # (time,)
-    lon   = f["longitude"][()]        # (y, x)
-    lat   = f["latitude"][()]         # (y, x)
+    disp = f["displacement"][()]
+    dates = f["date"][()]
+    lon   = f["longitude"][()]
+    lat   = f["latitude"][()]
 
 print("disp, dates shape:", disp.shape, dates.shape)
 
@@ -24,7 +26,7 @@ plt.ylabel("displacement (mm)")
 plt.tight_layout()
 plt.show()
 
-out_csv = "timeseries_pixel_100_120.csv"
+out_csv = data_dir / "timeseries_pixel_100_120.csv"
 with open(out_csv, "w", newline="") as fcsv:
     w = csv.writer(fcsv)
     w.writerow(["date", "disp_mm"])
